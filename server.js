5bccb458d197757d91a97c8e99435d13d3727ac9
Rafay <arafaymbir@gmail.com>
2020-04-23 19:47:16 -0400

    Weather app
    Added Backend code

--- server.js
+++ server.js
@@ -13,9 +13,38 @@
 //   res.end(index);
 // }).listen(9615);
 
+
+//3
+// var connect = require('connect');
+// var serveStatic = require('serve-static');
+
+// connect()
+//     .use(serveStatic(__dirname))
+//     .listen(8080, () => console.log('Server running on 8080...'));
+
+// //4
+// var fs = require("fs");
+// var host = "127.0.0.1";
+// var port = 8080;
+// var express = require("express");
+
+// var app = express();
+// app.use(express.static(__dirname + "/index.html")); //use static files in ROOT/public folder
+
+// app.get("/", function(request, response){ //root dir
+//     response.send("Hello!!");
+// });
+
+// app.listen(port, host);
+
+
+
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-connect()
-    .use(serveStatic(__dirname))
-    .listen(8080, () => console.log('Server running on 8080...'));
+var app = connect();
+
+app.use(serveStatic(__dirname, {'index': ['index.html']}));
+app.listen(8080);
+
+//hi rafay
